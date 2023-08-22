bc5446288da1502e8f8c5e76497882143f7bb745
Zoe Farrell <zoe@turing.edu>
2023-08-22 11:27:08 -0600

    Add project files.

---
+++ Mod4Week4Assessment/InterfacePractice.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Define the interface
+public interface InterfaceNameHere { }
+
+// Implement the interface in different vehicle classes
+public class Car : InterfaceNameHere
+{
+    public string Manufacturer { get; private set; }
+    public int Year { get; private set; }
+
+    public Car(string manufacturer, int year)
+    {
+        Manufacturer = manufacturer;
+        Year = year;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Car: Starting the engine...");
+    }
+
+    public void Accelerate()
+    {
+        Console.WriteLine("Car: Accelerating...");
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Car: Applying brakes...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car: Stopping the engine.");
+    }
+}
+
+public class Bicycle : InterfaceNameHere
+{
+    public string Manufacturer { get; private set; }
+    public bool HasBasket { get; private set; }
+
+    public Bicycle(string manufacturer, bool hasBasket)
+    {
+        Manufacturer = manufacturer;
+        HasBasket = hasBasket;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Bicycle: Pedaling...");
+    }
+
+    public void Accelerate()
+    {
+        Console.WriteLine("Bicycle: Pedaling faster...");
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Bicycle: Applying brakes...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Bicycle: Coming to a stop.");
+    }
+}
---
+++ Mod4Week4Assessment/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student
+{
+    public string Name { get; private set; }
+    public int Grade { get; private set; }
+    public DateTime Birthday { get; private set; }
+
+    public Student(string name, int grade, DateTime birthday)
+    {
+        Name = name;
+        Grade = grade;
+        Birthday = birthday;
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, I'm {Name}, a student in grade {Grade}!";
+    }
+
+    public string GetBirthday()
+    {
+        return $"{Name}'s birthday is on {Birthday:MMMM d, yyyy}";
+    }
+}
+
+public class Teacher
+{
+    public string Name { get; private set; }
+    public DateTime Birthday { get; private set; }
+    public string Subject { get; private set; }
+
+    public Teacher(string name, string subject, DateTime birthday)
+    {
+        Name = name;
+        Birthday = birthday;
+        Subject = subject;
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, I'm {Name}, a {Subject} teacher!";
+    }
+
+    public string GetBirthday()
+    {
+        return $"{Name}'s birthday is on {Birthday:MMMM d, yyyy}";
+    }
+}
+
+// Create a school class that uses dependency injection by taking in a list of people.
+public class School { }
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Your solution should make the following code run without errors!
+
+        //// Creating instances of Student and Teacher
+        //Student studentAlice = new Student("Alice", 10, new DateTime(2007, 1, 20));
+        //Student studentEthan = new Student("Ethan", 12, new DateTime(2005, 3, 10));
+        //Teacher teacherJohnson = new Teacher("Mrs. Johnson", "Math", new DateTime(1980, 12, 10));
+
+        //// Creating a list of people
+        //List<Person> people = new List<Person> { studentAlice, studentEthan, teacherJohnson };
+
+        //// Using dependency injection to create a School instance
+        //School school = new School(people);
+
+        //// List all birthdays
+        //school.ListBirthdays();
+    }
+}
