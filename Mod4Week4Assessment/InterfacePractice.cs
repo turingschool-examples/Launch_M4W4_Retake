40b2f5ee791944fcb24c154b15481420e65a551b
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-09-26 20:13:22 -0600

    Update for retake

--- Mod4Week4Assessment/InterfacePractice.cs
+++ Mod4Week4Assessment/InterfacePractice.cs
@@ -7,18 +7,62 @@ public interface InterfaceNameHere { }
 // Implement the interface in different vehicle classes
 public class Pig : InterfaceNameHere
 {
+	public bool IsHungry { get; set; }
+	public bool IsTired { get; set; }
+	
 	public Pig()
 	{
+		IsHungry = true;
+		IsTired = true;
+	}
 
+	public void Eat()
+	{
+		IsHungry = false;
+	}
+
+	public void Rest()
+	{
+		IsTired = false;
+	}
+
+	public string Play()
+	{
+		IsHungry = true;
+		IsTired = true;
+		return "I love to play in the mud!";
 	}
 
 }
 
 public class Horse : InterfaceNameHere
 {
-	public Horse()
+	public bool IsHungry { get; set; }
+	public bool IsTired { get; set; }
+	public string Rider { get; set; }
+	
+	public Horse(string rider)
 	{
-
+		IsHungry = true;
+		IsTired = true;
+		Rider = rider;
 	}
+
+	public void Eat()
+	{
+		IsHungry = false;
+	}
+
+	public void Rest()
+	{
+		IsTired = false;
+	}
+
+	public string Play()
+	{
+		IsHungry = true;
+		IsTired = true;
+		return $"I love to carry {Rider}";
+;	}
 
 }
